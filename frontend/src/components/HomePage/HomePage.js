d5900bb5c8b6ab0c24c10dee6bdae5b9cc24e4e2
Caio Lagreca <caiomiranda701@hotmail.com>
2024-01-10 17:28:39 +1000

    fixing router bug

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -12,7 +12,7 @@ function HomePage() {
                 Create posts to educate
               </span>
               <h2 className="max-w-2xl mt-12 mb-12 text-6xl 2xl:text-8xl text-white font-bold font-heading">
-                Pen down your ideas{" "}
+                Post your ideas{" "}
                 <span className="text-yellow-500">By creating a post</span>
               </h2>
               <p className="mb-12 lg:mb-16 2xl:mb-24 text-xl text-gray-100">
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-
+import { createBrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import store from "./redux/store/store";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    <App router={router} />
   </Provider>
 );
